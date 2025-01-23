63957dd28303c8ec67a6a9c45166a4324b53e51e
Hari Krishna <harikrishna99994@gmail.com>
2025-01-23 08:02:47 +0530

    Bubble Sort aand Selection Sort

---
+++ DSA/src/algorithms/sorts/insertionsort/InsertionSort.java
@@ -0,0 +1,27 @@
+/*
+ * package algorithms.sorts.insertionsort;
+ * 
+ * import java.util.Arrays;
+ * 
+ * public class InsertionSort {
+ * 
+ * public static void selectionSort(int[] array) {
+ * 
+ * 
+ * for (int i = 0; i < array.length; i++) { int minIndex = i; for (int j = i +
+ * 1; j < array.length; j++) {
+ * 
+ * if (array[minIndex] > array[j]) { minIndex = j; } }
+ * 
+ * int temp = array[i]; array[i] = array[minIndex]; array[minIndex] = temp; }
+ * 
+ * }
+ * 
+ * public static void main(String[] args) {
+ * 
+ * int[] myArray = {4, 2, 6, 5, 1, 3};
+ * 
+ * selectionSort(myArray);
+ * 
+ * System.out.println(Arrays.toString(myArray)); } }
+ */
